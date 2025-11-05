001c870db64de41c411c918e9654732695a43089
UMURERWLISAORNELLA <umurerwalisaornella@gmail.com>
2025-11-05 12:40:16 +0200

    first commit

---
+++ src/main/java/org/example/security_token/Config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package org.example.security_token.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .cors(Customizer.withDefaults())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/org/example/security_token/SecurityTokenApplication.java
@@ -0,0 +1,13 @@
+package org.example.security_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityTokenApplication.class, args);
+    }
+
+}
